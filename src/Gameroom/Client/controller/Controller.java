03d1559e00197d00704ea24a018d8ff0ea830a2b
nee <2102110703@student.kopo.ac.kr>
2022-12-12 09:54:39 +0900

    NEE 1212 0954

--- src/Gameroom/Client/controller/Controller.java
+++ src/Gameroom/Client/controller/Controller.java
@@ -70,36 +70,5 @@ public class Controller {
 		});
 	}
 
-//	public void enter() {
-//
-//		loginPanel = new LoginPanel();
-//		MainPanel.frame.change(currentPanel); // 로그인 패널
-//
-//		client = new MultiClient();
-//		String name = "임시 이름" + (int) (Math.random() * 10); // 구분자
-//		client.start(name); // 소켓 연결
-//	}
-//
-//	public void Lobby() {
-//
-//		currentPanel = new LobbyPanel();
-//		MainPanel.frame.change(currentPanel);
-//
-//	}
-
-//	public void login(String id, String pw) {
-//		String result;
-//
-////		  서버로 아이디 비밀번호를 보내주고 로그인~ 
-//		/*
-//		 * 
-//		 * if(result == "성공") {
-//		 * 
-//		 * 
-//		 * 
-//		 * }
-//		 */
-//
-//	}
 
 }
