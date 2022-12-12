0b73d63b33b3c999abb1a285afc68561ce87a463
nee <2102110703@student.kopo.ac.kr>
2022-12-12 10:58:58 +0900

    nee 1212 1058 model 수정

--- src/Gameroom/Client/model/ClientModel.java
+++ src/Gameroom/Client/model/ClientModel.java
@@ -2,18 +2,16 @@ package Gameroom.Client.model;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.rmi.UnknownHostException;
-import java.util.ArrayList;
 import java.util.HashMap;
+
 import Gameroom.Utility.DataDTO;
-import Gameroom.Utility.Info;
+import Gameroom.Utility.UserDAO;
+import Gameroom.Utility.UserDTO;
 
 //import Gameroom.Client.MultiClient.SendThread;
 
@@ -23,34 +21,47 @@ public class ClientModel implements Runnable {
 	BufferedWriter writer = null;
 	DataDTO dto = null;
 	String[] userInfoList = new String[5];
+	UserDAO userdao = new UserDAO();
 
 	public ClientModel() {
 
 	}
 
 	public void login(String id, String pw) {
-		try {
-			if (socket == null) {
-				socket = new Socket("localhost", 8000);
-				System.out.println("[서버와 연결되었습니다]");
-				writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-				reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+		UserDTO loginuser = userdao.loginCheck(id, pw);
+
+		if (loginuser == null) {
+			System.out.println("실패");
+
+		} else {
+			System.out.println("성공");
+			try {
+				if (socket == null) {
+					socket = new Socket("localhost", 8000);
+					
+					loginuser.setUserSocket(socket);
+					
+					System.out.println("[서버와 연결되었습니다]");
+					writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+					reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				}
+			} catch (UnknownHostException e) {
+				System.out.println("서버를 찾을 수 없습니다.");
+				e.printStackTrace();
+				System.exit(0);
+			} catch (IOException e) {
+				System.out.println("[서버 접속끊김]");
 			}
-		} catch (UnknownHostException e) {
-			System.out.println("서버를 찾을 수 없습니다.");
-			e.printStackTrace();
-			System.exit(0);
-		} catch (IOException e) {
-			System.out.println("[서버 접속끊김]");
-		}
-		try {
-			writer.write(id);
-			writer.write(pw);
-			writer.flush();
-			System.out.println("서버에 보낸 메시지 : " + id + "/"+pw);
-			System.out.println("서버에서 보낸 메시지 : " + reader.readLine());
-		} catch (IOException e) {
-			e.printStackTrace();
+			try {
+				writer.write(id);
+				writer.write(pw);
+				writer.flush();
+				System.out.println("서버에 보낸 메시지 : " + id + "/" + pw);
+				System.out.println("서버에서 보낸 메시지 : " + reader.readLine());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
