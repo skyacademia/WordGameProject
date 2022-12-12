fcefda78350adf2a434c4ee6379a9c3bb152c302
nee <2102110703@student.kopo.ac.kr>
2022-12-12 19:38:28 +0900

    nee 수정 1212 1938

--- src/Gameroom/Server/MultiServer.java
+++ src/Gameroom/Server/MultiServer.java
@@ -115,7 +115,7 @@ class GameHandlerObject extends Thread {
 					writer.flush();
 				} else if(dto.getCommand()==Info.SEND){
 					
-				} else if(dto.getCommand() == Info.FIND) {
+				} else if(dto.getCommand() == Info.MAKEROOM) {
 					// 상대 찾기 
 					
 					
