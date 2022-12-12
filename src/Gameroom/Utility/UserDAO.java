7f2e153fb0fa2027b6a9bc4f9f3778ff50668b3c
nee <2102110703@student.kopo.ac.kr>
2022-12-12 16:54:15 +0900

    nee dao 변수명 쓰실때^^!~ 의미를 담아주세요^^~

--- src/Gameroom/Utility/UserDAO.java
+++ src/Gameroom/Utility/UserDAO.java
@@ -16,7 +16,7 @@ public class UserDAO {
 	public UserDAO() {
 		try {
 			String dbURL = "jdbc:mysql://localhost:3306/game_server"; // localhost:3306 : 우리 서버에 설치된 mysql을 의미, /BBS : mysql안의 BBS라는 데이터 베이스에 접속할 수 있도록 함.
-			String dbID = "admin"; // dbID = "root" : root 계정에 접근할 수 있도록 함
+			String dbID = "root"; // dbID = "root" : root 계정에 접근할 수 있도록 함
 			String dbPassword = "1234"; // 패스워드나 ID를 다르게 입력하면 정상적으로 DB에 접속할 수 없음
 			Class.forName("com.mysql.cj.jdbc.Driver"); // Class.forName : mysql driver를 찾을 수 있도록 함 *Driver : mysql에 접속할 수 있도록 매개체 역할을 해주는 하나의 라이브러리 
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword); //conn : getConnection(db URL, dbID, dbPassword)를 이용하여 DB에 접속하고 접속이 완료가 되면 conn 객체안에 접속된 정보가 담긴다
@@ -27,24 +27,24 @@ public class UserDAO {
 
 	public UserDTO login(String userID, String userPassword) { // 어떤 계정에 대한 실제로 로그인을 시도하는 함수, 인자값으로 ID와 Password를 받아 login을 판단함.
 		String SQL = "SELECT * FROM user_info WHERE id = ? and pw=? "; // 실제로 DB에 입력될 명령어를 SQL 문장으로 만듬.
-		UserDTO y = new UserDTO();
+		UserDTO user = new UserDTO();
 		try {
 			pstmt = conn.prepareStatement(SQL);
 			pstmt.setString(1,  userID);
 			pstmt.setString(2,  userPassword);
 			rs = pstmt.executeQuery(); // 어떠한 결과를 받아오는 ResultSet 타입의 rs 변수에 쿼리문을 실행한 결과를 넣어줌 
 			while(rs.next()) {
-				y.setId(rs.getString(1));
-				y.setPw(rs.getString(2));
-				y.setWin(rs.getInt(3));
-				y.setLose(rs.getInt(4));
-				y.setTie(rs.getInt(5));
+				user.setId(rs.getString(1));
+				user.setPw(rs.getString(2));
+				user.setWin(rs.getInt(3));
+				user.setLose(rs.getInt(4));
+				user.setTie(rs.getInt(5));
 			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return y;
+		return user;
 	}
 	
 	public void create_table() {
